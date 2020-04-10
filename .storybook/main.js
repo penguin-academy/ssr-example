321794f52dbc2845430fb985829047818652121c
Björn Schmidtke <bs@pa1nd.de>
2020-04-09 23:51:04 -0400

    add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader')
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+
+    return config
+  }
+}
---
+++ stories/2-Example.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Example from '../src/components/Example'
+
+export default {
+  title: 'Example'
+}
+
+export const ExamplePage = () => <Example />
